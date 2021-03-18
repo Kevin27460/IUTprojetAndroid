44542704c56f8d35746e56972b193829184c6c12
kevin27460 <williams.kevin27460@gmail.com>
2021-03-18 12:05:42 +0100

    Ajout fonctionnalité pour aller sur l'interface graphique des niveaux

--- app/src/main/java/com/example/projetsimon/MainActivity.java
+++ app/src/main/java/com/example/projetsimon/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.projetsimon;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import android.widget.Button;
+import android.content.Intent;
+import android.view.View;
+
+
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
     public void openDifficulte() {
-        Intent intent = new Intent(this, Difficulte.class);
+        Intent intent = new Intent(this, NiveauxActivity.class);
         startActivity(intent);
     }
 }
