5b801eb433d883f7aedf24e9712eb5c66fac7c9d
kevin27460 <williams.kevin27460@gmail.com>
2021-03-18 11:49:42 +0100

    Ajout de l'interface graphique pour les difficultés

---
+++ app/src/main/java/com/example/projetsimon/NiveauxActivity.java
@@ -0,0 +1,15 @@
+package com.example.projetsimon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NiveauxActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_niveaux);
+
+    }
+}
