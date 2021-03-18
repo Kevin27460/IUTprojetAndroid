43c1bc67d00a194786ed845f28514742957e4c18
kevin27460 <williams.kevin27460@gmail.com>
2021-03-18 16:47:40 +0100

    liaison + modif graphique de jeu et NiveauxActivity

--- app/src/main/java/com/example/projetsimon/NiveauxActivity.java
+++ app/src/main/java/com/example/projetsimon/NiveauxActivity.java
@@ -2,14 +2,31 @@ package com.example.projetsimon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class NiveauxActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_niveaux);
 
+
+        button = (Button) findViewById(R.id.button_jouer);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDifficulte();
+            }
+        });
     }
+    public void openDifficulte() {
+        Intent intent = new Intent(this, JeuActivity.class);
+        startActivity(intent);
+    }
+
 }
