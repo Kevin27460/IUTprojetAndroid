717edbd5e9363cdd7c975a4e37c3ea495b00a791
kevin27460 <williams.kevin27460@gmail.com>
2021-03-18 17:12:31 +0100

    modif

--- app/src/main/java/com/example/projetsimon/JeuActivity.java
+++ app/src/main/java/com/example/projetsimon/JeuActivity.java
@@ -31,4 +31,71 @@ public class JeuActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+   /* nbrBloc = 4;
+    for(int i =0; i <= startSeq;i++)
+     sequenceTab = new Random().nextInt(nbrBloc)
+     maxSeq=startSeq;
+
+        nouvCouleur new Random().nextInt(nbrBloc);
+    sequenceTab [maxSeq] = nouvCouleur;
+    maxSeq ++;
+
+        for(int i=0; i<maxSeq;i++)
+        {
+         switch(sequenceTab[i]) {
+                    case 0:
+                         //allume le cube 1
+                    break;
+                    case 1:
+                         //allume le cube 2
+                    break;
+                    case 2:
+                         //allume le cube 3
+                    break;
+                    case 3:
+                         //allume le cube 4
+                    break;
+                    case 4:
+                         //allume le cube 5
+                    break;
+                    case 5:
+                         //allume le cube 6
+                    break;
+                    case 6:
+                        //allume le cube 7
+                   break;
+                   case 7:
+                        //allume le cube 8
+                  break;
+                  case 8:
+                       //allume le cube 9
+                 break;
+                 case 9:
+                       //allume le cube 10
+                 break;
+                 }
+            }
+        myButton.getBackground().setAlpha(255);
+
+            button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                verifSeq(numero du bloc);
+            }
+                    });
+                            }
+            public void verifSeq(int bloc) {
+                    if(sequenceTab[position]==bloc)
+                    position++;
+                    else
+                    vie--;
+                    position = 0;
+                    }
+
+
+                 */
+
+
+
+
 }
